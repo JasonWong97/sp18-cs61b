9cdce4accd63c84ecb5c9edd5b2c254361136040
wengjiacheng <ws940875538@gmail.com>
2020-12-05 20:28:28 +0800

    completed first part proj1a    1

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,14 @@
+package PACKAGE_NAME;/**
+ * @ClassName ArrayDeque
+ * @Description: TODO 
+ * @Author Jiacheng Weng
+ * @Date 12/4/20 9:44 下午
+ * @Version V1.0
+**/
+public class ArrayDeque {
+
+
+
+
+
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,11 @@
+/**
+ * @ClassName LinkedListDeque
+ * @Description: TODO
+ * @Author Jiacheng Weng
+ * @Date 11/30/20 1:52 下午
+ * @Version V1.0
+ **/
+public class LinkedListDeque {
+
+
+}
