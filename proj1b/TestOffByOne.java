22bf5c97b2f5584d3e8ccee4824738bde4375580
wengjiacheng <ws940875538@gmail.com>
2020-12-06 20:51:37 +0800

    completed first part proj1b    4

--- proj1b/TestOffByOne.java
+++ proj1b/TestOffByOne.java
@@ -12,8 +12,12 @@ public class TestOffByOne {
     // Your tests go here.
     @Test
     public void testOffByOne() {
-        Assert.assertFalse(offByOne.equalChars('a', 'b'));
-        Assert.assertTrue(offByOne.equalChars('a', 'a'));
+        assertTrue(offByOne.equalChars('a', 'b'));
+        assertTrue(offByOne.equalChars('r', 'q'));
+        assertFalse(offByOne.equalChars('a', 'e'));
+        assertFalse(offByOne.equalChars('z', 'a'));
+        assertFalse(offByOne.equalChars('a', 'a'));
+        assertTrue(offByOne.equalChars('%', '&'));
     }
 
 }
