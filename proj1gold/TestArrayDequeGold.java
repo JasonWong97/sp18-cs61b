7b6fc06ff16e2249f83814eeed8bb39fe2cb95eb
wengjiacheng <ws940875538@gmail.com>
2020-12-08 20:24:39 +0800

    completed first part proj1gold    8

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,49 +1,62 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
-/**
- * @ClassName TestArrayDequeGold
- * @Description: TODO
- * @Author Jiacheng Weng
- * @Date 12/8/20 7:07 下午
- * @Version V1.0
- **/
 public class TestArrayDequeGold {
-
     @Test
-    public void testArrayDequeGold() {
-        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+    public void testDeques() {
 
-//        addLast  &  removeLast
-        for (int i = 0; i < 10; i += 1) {
-            Integer num = StdRandom.uniform(100);
-            sad.addLast(num);
-            ads.addLast(num);
-        }
-        for (int i = 0; i < 10; i += 1) {
-            Integer actual = ads.removeLast();
-            Integer expected = sad.removeLast();
-            assertEquals("addLast(" + actual + ")\n"
-                    +"addLast(" + expected + ")\n"
-                    +"removeLast()\n", actual, expected);
-        }
+        int[] arr = new int[9];
 
+        for (int randCnt = 0; randCnt < 2000; randCnt++) {
+            StudentArrayDeque<Integer> deq = new StudentArrayDeque<>();
+            ArrayDequeSolution<Integer> deqsol = new ArrayDequeSolution<>();
+            for (int arrLen = 0; arrLen < arr.length; arrLen++) {
+                arr[arrLen] = (int) (Math.random() * 100);
+            }
 
-//        addFirst  &  removeFirst
-        for (int i = 0; i < 10; i += 1) {
-            Integer num = StdRandom.uniform(100);
-            sad.addFirst(num);
-            ads.addFirst(num);
+            String operations = "";
+            for (int arrLen = 0; arrLen < arr.length; arrLen++) {
+                if (arr[arrLen] < 25) {
+                    deq.addFirst(arr[arrLen]);
+                    deqsol.addFirst(arr[arrLen]);
+                    /*System.out.println(deqsol.get(0));
+                    System.out.println(deq.get(0));
+                    System.out.println();*/
+                    operations += "addFirst(" + arr[arrLen] + ") ";
+                    assertEquals(operations, deqsol.get(0), deq.get(0));
+                } else if (arr[arrLen] < 50) {
+                    deq.addLast(arr[arrLen]);
+                    deqsol.addLast(arr[arrLen]);
+                    /*System.out.println(deqsol.get(deqsol.size()-1));
+                    System.out.println(deq.get(deqsol.size()-1));
+                    System.out.println();*/
+                    operations += "addLast(" + arr[arrLen] + ") ";
+                    assertEquals(operations, deqsol.get(deqsol.size() - 1),deq.get(deqsol.size() - 1));
+                } else if (arr[arrLen] < 75 && deq.size() != 0) {
+                    operations += "removeFirst() ";
+                    if (deq.size() != 0) {
+                        Integer x = deq.removeFirst();
+                        Integer y = deqsol.removeFirst();
+                        /*System.out.println(x);
+                        System.out.println(y);
+                        System.out.println(operations);
+                        operations += "removeFirst() ";*/
+                        assertEquals(operations, y, x);
+                    }
+                } else {
+                    operations += "removeLast() ";
+                    if (deq.size() != 0) {
+                        Integer x = deq.removeLast();
+                        Integer y = deqsol.removeLast();
+                        /*System.out.println(x);
+                        System.out.println(y);
+                        System.out.println();
+                        operations += "removeLast() ";*/
+                        assertEquals(operations, y, x);
+                    }
+                }
+            }
+            //System.out.println(operations);
         }
-        for (int i = 0; i < 10; i += 1) {
-            Integer actual = ads.removeFirst();
-            Integer expected = sad.removeFirst();
-            assertEquals("addFirst(" + actual + ")\n"
-                    +"addFirst(" + expected + ")\n"
-                    +"removeFirst()\n", actual, expected);
-        }
-
     }
 }
