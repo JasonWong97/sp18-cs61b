db7fb1886728ba6284dfbab0d307e3b9366b8686
wengjiacheng <ws940875538@gmail.com>
2020-12-08 20:08:19 +0800

    completed first part proj1gold    4

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -2,40 +2,48 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+/**
+ * @ClassName TestArrayDequeGold
+ * @Description: TODO
+ * @Author Jiacheng Weng
+ * @Date 12/8/20 7:07 下午
+ * @Version V1.0
+ **/
 public class TestArrayDequeGold {
 
     @Test
-    public void testStudentArrayDeque() {
-        StudentArrayDeque<Integer> test1 = new StudentArrayDeque<>();
-        ArrayDequeSolution<Integer> test2 = new ArrayDequeSolution<>();
-        Integer studentOutput;
-        Integer correctOutput;
-        while (true) {
-            double numberBetweenZeroAndOne = StdRandom.uniform();
-            int number = StdRandom.uniform(10);
-            if (numberBetweenZeroAndOne > 0.75) {
-                test1.addFirst(number);
-                test2.addFirst(number);
-                System.out.printf("addFirst(%d)\n", number);
-            } else if (numberBetweenZeroAndOne >= 0.5 && numberBetweenZeroAndOne < 0.75) {
-                test1.addLast(number);
-                test2.addLast(number);
-                System.out.printf("addLast(%d)\n", number);
-            } else if (numberBetweenZeroAndOne < 0.5 && numberBetweenZeroAndOne >= 0.25
-                    && !test1.isEmpty() && !test2.isEmpty()) {
-                studentOutput = test1.removeFirst();
-                correctOutput = test2.removeFirst();
-                System.out.printf("removeFirst()\n");
-                assertEquals(correctOutput, studentOutput);
-            } else if (numberBetweenZeroAndOne >= 0 && numberBetweenZeroAndOne < 0.25
-                    && !test1.isEmpty() && !test2.isEmpty()) {
-                studentOutput = test1.removeLast();
-                correctOutput = test2.removeLast();
-                System.out.printf("removeLast()\n");
-                assertEquals(correctOutput, studentOutput);
-            } else  {
-                assertEquals(test2.isEmpty(), test1.isEmpty());
-            }
+    public void testArrayDequeGold() {
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+
+//        addLast  &  removeLast
+        for (int i = 0; i < 10; i += 1) {
+            Integer num = StdRandom.uniform(100);
+            sad.addLast(num);
+            ads.addLast(num);
         }
+        for (int i = 0; i < 10; i += 1) {
+            Integer actual =ads.removeLast();
+            Integer expected =sad.removeLast();
+            System.out.println("addFirst("+actual+")\n");
+            System.out.println("addFirst("+expected+")\n");
+            assertEquals("removeLast()\n",actual,expected);
+        }
+
+
+//        addFirst  &  removeFirst
+        for (int i = 0; i < 10; i += 1) {
+            Integer num = StdRandom.uniform(100);
+            sad.addFirst(num);
+            ads.addFirst(num);
+        }
+        for (int i = 0; i < 10; i += 1) {
+            Integer actual =ads.removeFirst();
+            Integer expected =sad.removeFirst();
+            System.out.println("addFirst("+actual+")\n");
+            System.out.println("addFirst("+expected+")\n");
+            assertEquals("removeFirst()\n",actual,expected);
+        }
+        
     }
 }
