8a261624e82a401aa155add84a5f066ef9910620
wengjiacheng <ws940875538@gmail.com>
2020-12-11 18:58:57 +0800

    completed first part proj2    1

---
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -0,0 +1,14 @@
+package synthesizer;/**
+ * @ClassName AbstractBoundedQueue
+ * @Description: TODO 
+ * @Author Jiacheng Weng
+ * @Date 12/11/20 12:18 上午
+ * @Version V1.0
+**/
+public class AbstractBoundedQueue {
+
+
+
+
+
+}
